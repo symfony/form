1f0995fba634315fbcb39f956c22183b042b5f66
Nicolas Grekas <nicolas.grekas@gmail.com>
2025-09-11 10:16:56 +0200

    Replace __sleep/wakeup() by __(un)serialize() for throwing and internal usages

--- Extension/DataCollector/FormDataCollector.php
+++ Extension/DataCollector/FormDataCollector.php
@@ -200,10 +200,7 @@ class FormDataCollector extends DataCollector implements FormDataCollectorInterf
         return $this->data;
     }
 
-    /**
-     * @internal
-     */
-    public function __sleep(): array
+    public function __serialize(): array
     {
         foreach ($this->data['forms_by_hash'] as &$form) {
             if (isset($form['type_class']) && !$form['type_class'] instanceof ClassStub) {
@@ -211,9 +208,7 @@ class FormDataCollector extends DataCollector implements FormDataCollectorInterf
             }
         }
 
-        $this->data = $this->cloneVar($this->data);
-
-        return parent::__sleep();
+        return ['data' => $this->data = $this->cloneVar($this->data)];
     }
 
     protected function getCasters(): array
