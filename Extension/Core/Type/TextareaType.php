5188efec153f1c1239060c66ca3d0ca0a6de0814
Christian Flothmann <christian.flothmann@open.de>
2026-01-22 16:21:26 +0100

    don't skip custom view transformers while normalizing submitted newlines

--- Extension/Core/Type/TextareaType.php
+++ Extension/Core/Type/TextareaType.php
@@ -12,19 +12,20 @@
 namespace Symfony\Component\Form\Extension\Core\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\EventListener\CrlfNormalizerListener;
+use Symfony\Component\Form\DataTransformerInterface;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\Util\StringUtil;
 
-class TextareaType extends AbstractType
+class TextareaType extends AbstractType implements DataTransformerInterface
 {
     /**
      * @return void
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->addEventSubscriber(new CrlfNormalizerListener());
+        $builder->addViewTransformer($this);
     }
 
     /**
@@ -44,5 +45,27 @@ class TextareaType extends AbstractType
     public function getBlockPrefix(): string
     {
         return 'textarea';
+    }
+
+    public function transform(mixed $value): mixed
+    {
+        if (null === $value) {
+            return '';
+        }
+
+        return $value;
+    }
+
+    public function reverseTransform(mixed $value): mixed
+    {
+        if (!\is_string($value)) {
+            return $value;
+        }
+
+        if ('' === $value) {
+            return null;
+        }
+
+        return StringUtil::normalizeNewlines($value);
     }
 }
